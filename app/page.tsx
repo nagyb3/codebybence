84b9224ce66e366753202f34373c63f22d0f0e19
nagyb3 <nagybence2003@gmail.com>
2023-06-25 20:25:31 +0200

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,14 @@ export default function Home() {
       <nav className='border-b border-grey-500 px-5 py-5'>
       <ModeToggle />
       </nav>
-      <h1>hi this is home</h1>
+      <main>
+        <div className='max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32'>
+          <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            Hey, it's me!
+          </h1>
+          <p className='text-center pt-10'>i make some websites...</p>
+        </div>
+      </main>
     </div>
   )
 }
