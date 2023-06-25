d796cda1214ab158a87d9ccc0e68694a9a8e0b8d
nagyb3 <nagybence2003@gmail.com>
2023-06-25 20:30:32 +0200

    fix &apos escaping issue

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main>
         <div className='max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32'>
           <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
-            Hey, it's me!
+            Hey, it&apos;s me!
           </h1>
           <p className='text-center pt-10'>i make some websites...</p>
         </div>
