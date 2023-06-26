e48c25cd3ff6fdd1002afca04490ae03e2e90c3c
nagyb3 <nagybence2003@gmail.com>
2023-06-26 16:27:39 +0200

    About me and Github Profile Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { ModeToggle } from '@/components/mode-toggle'
 import { ProjectCard } from '@/components/project-card'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -11,13 +12,16 @@ export default function Home() {
       </nav>
       <main>
         <div className='max-w-5xl mx-auto pt-20 pb-20 sm:pt-24 lg:pt-32'>
-          <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+          <h1 className="text-slate-900 font-extrabold text-5xl sm:text-6xl lg:text-7xl tracking-tight text-center dark:text-white">
             Hey, it&apos;s me!
           </h1>
-          <p className='text-center pt-10'>i make some websites...</p>
+          <p className='text-center pt-10 text-xl'>i make some websites...</p>
+          <div className='text-center pt-5'>
+            <Button variant="link">My GitHub Profile</Button>
+          </div>
         </div>
         <div className='pt-20 pb-20 max-w-5xl mx-auto'>
-          <p className='text-slate-900 text-center font-bold text-2xl sm:text-xl lg:text-l dark:text-white'>
+          <p className='text-slate-900 text-center font-bold text-3xl sm:text-2xl lg:text-xl dark:text-white'>
             Projects I have built:
           </p>
           <div className='grid grid-cols-1 gap-3 justify-items-center py-20 md:grid-cols-2'>
@@ -25,6 +29,14 @@ export default function Home() {
             githubLink="https://github.com/nagyb3/social-media-clone" siteLink="https://nagyb.xyz" />
             <ProjectCard name="tic-tac-toe" description="Really simple tic-tac-toe made with some Javascript"
             githubLink="https://github.com/nagyb3/tic-tac-toe" siteLink="https://nagyb3.github.io/tic-tac-toe/" />
+          </div>
+        </div>
+        <div className='max-w-3xl pb-20 mx-auto'>
+          <h2 className='text-3xl font-bold sm:text-2xl lg:text-xl text-center pb-10'>
+            About me:
+          </h2>
+          <div className='text-justify'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, quam consectetur enim pariatur expedita sint blanditiis dolorum sed. Laboriosam et vero doloribus sequi perspiciatis necessitatibus debitis commodi dolor ex cumque ratione nemo explicabo, voluptatibus eius quam delectus neque. Quam, dolorum.
           </div>
         </div>
       </main>
