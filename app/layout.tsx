e34165efc9eb24ba6c1e94e2e7e2434544a9a21b
Bence Nagy <bence@veldex.at>
2025-06-21 18:07:57 +0200

    Add umami tracking script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body></body>
+      <head>
+        <script
+          defer
+          src="https://umami-production-3219.up.railway.app/script.js"
+          data-website-id="dd831adb-205a-437d-b8d8-0ad408002d6f"
+        ></script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
